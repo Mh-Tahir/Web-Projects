d0f967f13bb29c24a85e3829e4479f73b34ba6ac
Mh-Tahir <58627780+Mh-Tahir@users.noreply.github.com>
2020-08-23 00:24:51 +0300

    Update doggos.js
    added choosing breeds

--- Dog_App/doggos.js
+++ Dog_App/doggos.js
@@ -1,17 +1,44 @@
-const BREEDS_URL = "https://dog.ceo/api/breeds/image/random";
+const BREEDS_URL = "https://dog.ceo/api/breeds/list/all";
+const select = document.querySelector('#breed');
 
-function addDoggo (){
-    fetch(BREEDS_URL)
-      .then(function(response) {
-        return response.json();
-      })
-      .then(function(data) {
-        const img = document.createElement('img');
-        img.src = data.message;
-        img.alt = 'Cute doggo';
-    
-        document.querySelector('.doggos').appendChild(img);
-      });
-}
+fetch(BREEDS_URL)
+  .then(response => {
+    return response.json();
+  })
+  .then(data => {
+    const breedsObject = data.message;
+    const breedsArray = Object.keys(breedsObject);
 
-document.querySelector('.add-doggo').addEventListener('click', addDoggo);
+    for (let i = 0; i < breedsArray.length; i++) {
+      const option = document.createElement('option');
+      option.value = breedsArray[i];
+      option.innerText = breedsArray[i];
+      select.appendChild(option);
+    }
+  })
+
+select.addEventListener('change', function(event) {
+  let url = `https://dog.ceo/api/breed/${event.target.value}/images/random`;
+
+  getDoggo(url);
+});
+
+const img = document.querySelector('.main-image');
+const spinner = document.querySelector('.loading-dog');
+
+function getDoggo (url) {
+  spinner.classList.add('show');
+  img.classList.remove('show');
+  fetch(url)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      img.src = data.message;
+    })
+  }
+  
+img.addEventListener('load', function () {
+  spinner.classList.remove('show');
+  img.classList.add('show');    
+})
