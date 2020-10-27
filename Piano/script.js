bf93be7c3d5b46b6198daf02d0a295f5ca29bf49
Mh-Tahir <tahirmr@mail.ru>
2020-10-27 17:30:03 +0300

    Better version

--- Piano/script.js
+++ Piano/script.js
@@ -1,125 +1,28 @@
-document.addEventListener("keydown", function (event) {
-  if (event.code === "KeyA") {
-    let a = new Audio("A.mp3");
-    a.play();
-  } else if (event.code === "KeyS") {
-    let s = new Audio("S.mp3");
-    s.play();
-  } else if (event.code === "KeyD") {
-    let d = new Audio("D.mp3");
-    d.play();
-  } else if (event.code === "KeyF") {
-    let f = new Audio("F.mp3");
-    f.play();
-  } else if (event.code === "KeyG") {
-    let g = new Audio("G.mp3");
-    g.play();
-  } else if (event.code === "KeyH") {
-    let h = new Audio("H.mp3");
-    h.play();
-  } else if (event.code === "KeyJ") {
-    let j = new Audio("J.mp3");
-    j.play();
-  } else if (event.code === "KeyW") {
-    let d = new Audio("W.mp3");
-    d.play();
-  } else if (event.code === "KeyE") {
-    let f = new Audio("E.mp3");
-    f.play();
-  } else if (event.code === "KeyT") {
-    let g = new Audio("T.mp3");
-    g.play();
-  } else if (event.code === "KeyY") {
-    let h = new Audio("Y.mp3");
-    h.play();
-  } else if (event.code === "KeyU") {
-    let j = new Audio("U.mp3");
-    j.play();
+function pressKey(event) {
+  let keys = {
+    "KeyA": "A", "KeyS": "S", "KeyD": "D", "KeyF": "F", "KeyG": "G", "KeyH": "H", "KeyJ": "J",
+    "KeyW": "W", "KeyE": "E", "KeyT": "T", "KeyY": "Y", "KeyU": "U"
+  };
+  if (event.code in keys) {
+    let audio = new Audio("sounds/" + keys[event.code] + ".mp3");
+    audio.play();
   } else {
-    console.log("Warning! Wrong key is pressed.");
+    console.log("Wrong key is pressed.")
   }
+}
+
+function clickKey(event) {
+  let keys = ["A", "S", "D", "F", "G", "H", "J", "W", "E", "T", "Y", "U"];
+  if (keys.includes(event.target.innerText)) {
+    let audio = new Audio("sounds/" + event.target.innerText + ".mp3");
+    audio.play();
+  }
+}
+
+document.addEventListener("keydown", event => {
+  pressKey(event);
 });
 
-document.addEventListener("click", function (event) {
-  if (event.target.innerText === "A") {
-    let a = new Audio("A.mp3");
-    a.play();
-  } else if (event.target.innerText === "S") {
-    let s = new Audio("S.mp3");
-    s.play();
-  } else if (event.target.innerText === "D") {
-    let d = new Audio("D.mp3");
-    d.play();
-  } else if (event.target.innerText === "F") {
-    let f = new Audio("F.mp3");
-    f.play();
-  } else if (event.target.innerText === "G") {
-    let g = new Audio("G.mp3");
-    g.play();
-  } else if (event.target.innerText === "H") {
-    let h = new Audio("H.mp3");
-    h.play();
-  } else if (event.target.innerText === "J") {
-    let j = new Audio("J.mp3");
-    j.play();
-  } else if (event.target.innerText === "W") {
-    let d = new Audio("W.mp3");
-    d.play();
-  } else if (event.target.innerText === "E") {
-    let f = new Audio("E.mp3");
-    f.play();
-  } else if (event.target.innerText === "T") {
-    let g = new Audio("T.mp3");
-    g.play();
-  } else if (event.target.innerText === "Y") {
-    let h = new Audio("Y.mp3");
-    h.play();
-  } else if (event.target.innerText === "U") {
-    let j = new Audio("U.mp3");
-    j.play();
-  } else {
-    console.log("Warning! Wrong key is pressed.");
-  }
-});
-
-document.addEventListener("touchstart", function (event) {
-  if (event.target.innerText === "A") {
-    let a = new Audio("A.mp3");
-    a.play();
-  } else if (event.target.innerText === "S") {
-    let s = new Audio("S.mp3");
-    s.play();
-  } else if (event.target.innerText === "D") {
-    let d = new Audio("D.mp3");
-    d.play();
-  } else if (event.target.innerText === "F") {
-    let f = new Audio("F.mp3");
-    f.play();
-  } else if (event.target.innerText === "G") {
-    let g = new Audio("G.mp3");
-    g.play();
-  } else if (event.target.innerText === "H") {
-    let h = new Audio("H.mp3");
-    h.play();
-  } else if (event.target.innerText === "J") {
-    let j = new Audio("J.mp3");
-    j.play();
-  } else if (event.target.innerText === "W") {
-    let d = new Audio("W.mp3");
-    d.play();
-  } else if (event.target.innerText === "E") {
-    let f = new Audio("E.mp3");
-    f.play();
-  } else if (event.target.innerText === "T") {
-    let g = new Audio("T.mp3");
-    g.play();
-  } else if (event.target.innerText === "Y") {
-    let h = new Audio("Y.mp3");
-    h.play();
-  } else if (event.target.innerText === "U") {
-    let j = new Audio("U.mp3");
-    j.play();
-  } else {
-    console.log("Warning! Wrong key is pressed.");
-  }
+document.addEventListener("click", event => {
+  clickKey(event);
 });
